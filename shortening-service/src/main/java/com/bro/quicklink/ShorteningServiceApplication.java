fc1b3d9b65e86a20a09c741cab8d27eca1804b26
srpcmx <srpc.mx@gmail.com>
2025-08-28 19:36:21 -0600

    Delete unused ShorteningServiceApplication.java in shortening-service package and delete ComponentScan annotation from ShorteningServiceApplication

--- shortening-service/src/main/java/com/bro/quicklink/ShorteningServiceApplication.java
+++ shortening-service/src/main/java/com/bro/quicklink/ShorteningServiceApplication.java
@@ -2,10 +2,8 @@ package com.bro.quicklink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.bro.quicklink"})
 public class ShorteningServiceApplication {
 
     public static void main(String[] args) {
