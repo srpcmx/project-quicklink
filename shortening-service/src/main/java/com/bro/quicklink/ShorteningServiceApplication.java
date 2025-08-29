3f4b670679ab5131f46195070e3f54127b35e101
srpcmx <srpc.mx@gmail.com>
2025-08-28 19:23:40 -0600

    Add ComponentScan annotation to ShorteningServiceApplication

--- shortening-service/src/main/java/com/bro/quicklink/ShorteningServiceApplication.java
+++ shortening-service/src/main/java/com/bro/quicklink/ShorteningServiceApplication.java
@@ -2,8 +2,10 @@ package com.bro.quicklink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bro.quicklink"})
 public class ShorteningServiceApplication {
 
     public static void main(String[] args) {
